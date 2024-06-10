119c1ed8507bc2646e628a2615078f99a3c29000
Darien Romero <darien.romero@mediabyte.com.pe>
2024-06-10 12:11:06 -0500

    Firebase key

--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -12,7 +12,7 @@ export class StorageService {
         // Create a new instance of the Storage class
         this.storage = new Storage({
             projectId: 'apkmanager-12227',
-            keyFilename: './assets/apkmanager-12227-dfe1b13f205a.json'
+            keyFilename: process.env.FIREBASE_KEY_ROUTE
           });
       }
 
